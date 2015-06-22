11e71413d7fb5c8fa8294121a3bb42b6a173a8e3
Guillaume Duclos-Cianci <guillaume.ducloscianci@gmail.com>
2015-06-22 09:03:39 -0400

    Correction to objective 4.2: added script input argument

--- objective4.2/system_tap.sh
+++ objective4.2/system_tap.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -x
 
+cd `dirname $0`
+
 make
 
 prog_name=clock_gettime
+sample_size=$1
 
 echo "probe syscall.getuid {}" > ${prog_name}.stp
 echo "probe syscall.getuid.return {}" >> ${prog_name}.stp
-stap -g --suppress-time-limits -v ${prog_name}.stp -c "./$prog_name"
+stap -g --suppress-time-limits -v ${prog_name}.stp -c "./$prog_name -s $sample_size"
 
