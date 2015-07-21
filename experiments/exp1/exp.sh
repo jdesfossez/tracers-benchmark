1e03c7a6b8ee7cdc711632fa81e9a9f1e630897e
Guillaume Duclos-Cianci <guillaume.ducloscianci@gmail.com>
2015-07-21 13:49:43 -0400

    Setup for experiment 1

---
+++ experiments/exp1/exp.sh
@@ -0,0 +1,41 @@
+#/bin/bash -x
+
+cd $(dirname $0)
+exp_path=$(pwd)
+
+function run_scan {
+	scan_delay_path=../../scripts/scan_delay/v2.1
+	cp scan.param $scan_delay_path/
+	./$scan_delay_path/scan_delay.sh
+	cp $scan_delay_path/scanned.param .
+}
+
+function run_lttng {
+	lttng_path=../../scripts/lttng/v5.2
+	cp lttng.param $lttng_path
+	./$lttng_path/lttng.sh
+	mv $lttng_path/getuid_pthread_lttng.csv .
+}
+
+function run_plot_R {
+	R_path=../../R_analysis/lost_events/v3.0
+	cp getuid_pthread_lttng.csv $R_path/results
+	cd $R_path
+	Rscript plot_data.R
+	cd $exp_path
+	cp -r $R_path/results/figs .
+}
+
+if [ "$1" == "scan" ]; then
+	run_scan
+elif [ "$1" == "lttng" ]; then
+	run_lttng
+elif [ "$1" == "plot_R" ]; then
+	run_plot_R
+elif [ "$1" == "full" ]; then
+	run_scan
+	run_lttng
+	run_plot_R
+else
+	echo "add argument \"scan\", \"lttng\", \"plot_R\" or \"full\""
+fi
