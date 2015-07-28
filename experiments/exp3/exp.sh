13a5130902bfcfd54f52b93c400ced7fff9f7513
Guillaume Duclos-Cianci <guillaume.ducloscianci@gmail.com>
2015-07-28 12:49:16 -0400

    Prepared experiments exp3

---
+++ experiments/exp3/exp.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+benchmark_path=../../benchmarks/lttng/v1.0
+exp_path=$(pwd)
+
+cp calibrate.param $benchmark_path/calibration/
+cp lttng.param $benchmark_path/lttng/
+
+cd $benchmark_path
+./benchmark.sh
+
+cd $exp_path
+cp -r $benchmark_path/calibration .
+cp -r $benchmark_path/lttng .
