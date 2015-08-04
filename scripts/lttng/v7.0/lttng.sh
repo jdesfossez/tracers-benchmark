ec0e30e58ae9cccb4ced2d98fc2252636b84f6ee
Guillaume Duclos-Cianci <guillaume.ducloscianci@gmail.com>
2015-08-04 09:56:04 -0400

    Completed scripts lttng v7.0

---
+++ scripts/lttng/v7.0/lttng.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+function compute_trace_size {
+	total_trace_size=0
+	for cpu in $(seq 0 $cpu_max); do
+		trace_size=$(ls -l traces/kernel | grep channel0_$cpu$ | tr -s " " | cut -d" " -f5)
+		total_trace_size=$(($total_trace_size + $trace_size))
+	done
+	total_trace_size=$(($total_trace_size / 1024))
+}
+
+function save_stats {
+	mean=($(grep mean < statistics))
+	std=($(grep std < statistics))
+	echo "${mean[1]},${std[1]},$no_lost_events,$no_events_expected,$total_trace_size,$delay,$no_thread,$sample_size,$no_repetitions" >> ${prog_name}_${tracer_name}.csv
+}
+
+function clean {
+	rm -r traces/*
+	rm sample
+	rm statistics
+}
+
+function run {
+	lttng create bm_session -o traces/
+	lttng enable-channel -k channel0
+	lttng enable-event --kernel --syscall $syscall -c channel0
+	lttng add-context -k -t pid -c channel0
+	lttng start
+	echo $cmd
+	pid=$($cmd | cut -d " " -f2)
+	lttng stop
+	echo $(lttng view 2>&1 >/dev/null | grep discarded | cut -d" " -f4 | tr "\n" "+"| sed "s/.$//") | bc
+	lttng destroy
+	no_events=$(babeltrace traces/kernel/ 2>/dev/null | grep $syscall | grep "pid = $pid" | wc -l)
+	no_lost_events=$(( $no_events_expected - $no_events ))
+	compute_trace_size
+	save_stats
+	clean
+}
+
+function process_results {
+	Rscript average_results.R $no_repetitions
+	rm getuid_pthread_lttng_*.csv
+}
+
+cd $(dirname $0)
+prog_name=getuid_pthread
+tracer_name=lttng
+syscall=getuid
+no_cpus=$(nproc)
+cpu_max=$(($no_cpus - 1))
+#read parameters
+no_threads=($(grep no_thread < ${tracer_name}.param))
+no_threads=${no_threads[@]:1}
+sample_sizes=($(grep sample_size < ${tracer_name}.param))
+sample_sizes=${sample_sizes[@]:1}
+delays=($(grep delay < ${tracer_name}.param))
+delays=${delays[@]:1}
+no_repetitions=($(grep no_repetition < ${tracer_name}.param))
+no_repetitions=${no_repetitions[@]:1}
+
+
+change_cpus_governor_cmd=../../change_cpus_governor/v1.0/change_cpus_governor.sh
+prog_path=../../../load/getuid_pthread/v2.0/
+make -C $prog_path
+mv $prog_path/getuid_pthread .
+#Create directory where traces are written
+if [ ! -d traces ]; then
+	mkdir traces
+fi
+$change_cpus_governor_cmd performance
+#perform experiment
+for t in $(seq 1 $no_repetitions); do
+	#print to file csv field names
+	echo "mean,std,no_lost_events,no_events_expected,trace_size,delay,no_thread,sample_size,no_repetitions" > ${prog_name}_${tracer_name}.csv
+	for no_thread in $no_threads; do
+		for sample_size in $sample_sizes; do
+			for delay in $delays; do
+				cmd="./$prog_name -d $delay -s $sample_size -t $no_thread"
+				no_events_expected=$((2 * $sample_size + 2 * $no_thread))
+				run
+			done
+		done
+	done
+	mv getuid_pthread_lttng.csv getuid_pthread_lttng_$t.csv
+done
+rm -r traces
+rm getuid_pthread
+process_results
+$change_cpus_governor_cmd powersave
